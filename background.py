21b4e75c694cb4b601a8cb47602f9de0aa455824
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-01-17 03:14:18 +0500

    feat: cache background image

--- background.py
+++ background.py
@@ -3,13 +3,28 @@ from settings import *
 
 
 class Background:
+    _image_cache = {}  # Class-level cache for loaded images
+    
     def __init__(self, window_size):
         self.w, self.h = window_size
-        self.image = image.load(
-            "assets/background.jpg",
-            size=(self.w, self.h),
-            convert="default",
-        )
+        cache_key = f"{self.w}x{self.h}"
+        
+        # Try to get from cache first
+        if cache_key in Background._image_cache:
+            self.image = Background._image_cache[cache_key]
+        else:
+            # Load and cache if not present
+            self.image = image.load(
+                "assets/background.jpg",
+                size=(self.w, self.h),
+                convert="default",
+            )
+            Background._image_cache[cache_key] = self.image
+            
+            # Clear cache if it gets too large (keep last 3 sizes)
+            if len(Background._image_cache) > 3:
+                oldest_key = list(Background._image_cache.keys())[0]
+                del Background._image_cache[oldest_key]
 
     def draw(self, surface):
         image.draw(
